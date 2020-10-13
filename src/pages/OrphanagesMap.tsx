a950266a0f73f76b94e9b7a2103f08f1970e092f
Amanda <amandavieirazevedo@gmail.com>
2020-10-13 14:10:04 -0300

    adding map to orphanage page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import mapMarkerImg from "../images/map-marker.svg";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
-import '../styles/pages/orphanages-map.css';
+import { Map, TileLayer } from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
+import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
   return (
@@ -18,7 +20,15 @@ function OrphanagesMap() {
           <span>Minas Gerais</span>
         </footer>
       </aside>
-      <div></div>
+        <Map
+          center={[-19.9342715, -43.953214]}
+          zoom={15}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <TileLayer
+            url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          />
+        </Map>
       <Link to="#" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
