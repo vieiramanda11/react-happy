6bd74e5819af8d8dc939b26d9472557994eb3c62
Amanda <amandavieirazevedo@gmail.com>
2020-10-12 21:30:09 -0300

    creating landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/Logo.svg';
+import { FiArrowRight } from 'react-icons/fi';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Testing</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="logo happy"></img>
+
+        <main>
+          <h1>Bring happiness to the world</h1>
+          <p>Visit orphanages and change many children's day.</p>
+        </main>
+        <div className="location">
+          <strong>Belo Horizonte</strong>
+          <span>Minas Gerais</span>
+        </div>
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </a>
+      </div>
     </div>
   );
 }
